97e5e269abf1dfddc5cd4e985bf99c963f97b2d7
saul-melo <melo.saul123@icloud.com>
2021-03-01 23:47:58 -0600

    insert app here

---
+++ Bay/BayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BayApp.swift
+//  Bay
+//
+//  Created by Saul Melo on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bay
+//
+//  Created by Saul Melo on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Insert app here")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
